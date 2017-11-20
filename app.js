3534e5a8a6c1e866348a2bb1127470d41c3d55e9
André Carvalho <andre.carvalho@pt.softinsa.com>
2017-11-20 21:59:28 +0000

    Documentation.

--- app.js
+++ app.js
@@ -1,35 +1,55 @@
+// This code is for the backend part of the application.
+// It takes care of configuring connections to Watson and receiving messages
+// from the web page.
+
 "use strict";
 
-// Config
+const path = require("path");
+
+// Configuration. This uses the ".env" file, it reads variables from it
+// and saves them in process.env.
 const dotenv = require("dotenv");
 
 // @ts-ignore
 dotenv.config({ silent: true });
 
+// When running on Bluemix, we're running using Cloud Foundry.
+// cfenv allows us easy access to configuration variables from services
+// that we added to our app.
 const cfenv = require("cfenv");
+
+// Express is our web server. It's easy to configure and extremely powerful.
+const express = require("express");
+
+// We'll use this to read data sent by the web page.
+const bodyParser = require("body-parser");
+
+// Simple function that allows us to get weather forecasts
+// from Yahoo! Weather for a city.
+const getWeather = require("yahoo-weather");
+
+// Watson Conversation SDK.
+const ConversationV1 = require("watson-developer-cloud/conversation/v1");
+
+// Configuration for Watson's conversation SDK
+const conversationConfig = {
+    username: process.env.WATSON_CONVERSATION_USERNAME,
+    password: process.env.WATSON_CONVERSATION_PASSWORD,
+    version_date: ConversationV1.VERSION_DATE_2017_05_26
+};
+
+const conversationId = process.env.WATSON_CONVERSATION_WORKSPACE_ID;
+
+// Get the services from Cloud Foundry (cfenv). This fails silently if
+// we're not running on Bluemix.
 
 // @ts-ignore
 const appEnv = cfenv.getAppEnv();
 
 const services = appEnv.getServices();
 
-const path = require("path");
-
-const express = require("express");
-const bodyParser = require("body-parser");
-
-const getWeather = require("yahoo-weather");
-
-const ConversationV1 = require("watson-developer-cloud/conversation/v1");
-
 const watsonConversationServiceName =
     process.env.WATSON_CONVERSATION_SERVICE_NAME;
-
-const conversationConfig = {
-    username: process.env.WATSON_CONVERSATION_USERNAME,
-    password: process.env.WATSON_CONVERSATION_PASSWORD,
-    version_date: ConversationV1.VERSION_DATE_2017_05_26
-};
 
 // Use conversation credentials from cfenv when available.
 if (services[watsonConversationServiceName]) {
@@ -38,18 +58,30 @@ if (services[watsonConversationServiceName]) {
     Object.assign(conversationConfig, credentials);
 }
 
+// This object will be used to talk to Watson.
 const conversation = new ConversationV1(conversationConfig);
 
+// Create the server and configure it.
 const app = express();
 
+// Any file inside the "public" folder will be served as-is.
 app.use(express.static(path.join(__dirname, "public")));
+
+// Allow Express to read data from the page.
 app.use(bodyParser.json());
 
 /**
+ * Parses the matched entities from Watson and gets the weather for a city,
+ * along any messages.
  *
  * @param {Array<{ entity: string, value: string, confidence: number }>} entities
+ * @returns {Promise<{ messages: string[], weatherReport: any }>}
  */
 async function getWeatherReport(entities) {
+    // Entities is an array. We want to extract the time and
+    // the sys-location entities from it.
+    // We convert the array into an object on the process,
+    // makes things easier to work with.
     const { location, time } = entities.reduce(
         (acc, e) => {
             if (e.entity === "Time") {
@@ -66,10 +98,13 @@ async function getWeatherReport(entities) {
     );
 
     const result = {
+        // To hold any messages that may occur.
         messages: [],
+        // To hold the weather report.
         weatherReport: null
     };
 
+    // I'll leave this as an exercise for the reader :)
     if (time && location) {
         result.messages.push(
             `Sorry, I don't know how to work with time yet. But, here's the weather for ${
@@ -78,6 +113,7 @@ async function getWeatherReport(entities) {
         );
     }
 
+    // Get the weather for the location.
     if (location) {
         result.weatherReport = await getWeather(location);
     }
@@ -85,11 +121,14 @@ async function getWeatherReport(entities) {
     return result;
 }
 
-const conversationId = process.env.WATSON_CONVERSATION_WORKSPACE_ID;
-
+// This endpoint will respond to messages from the page.
 app.post("/api/message", (req, res) => {
+    // Get the text, and the Watson context from the request.
+    // The context is needed so that Watson knows the string of messages
+    // that led here.
     const { text, context } = req.body;
 
+    // Send a message to watson, and wait for the response.
     conversation.message(
         {
             input: {
@@ -100,7 +139,10 @@ app.post("/api/message", (req, res) => {
         },
         (err, response) => {
             if (err) {
+                // Error handling.
+
                 console.error(err);
+
                 res.status(500).json({
                     output: {
                         text: [
@@ -109,24 +151,29 @@ app.post("/api/message", (req, res) => {
                     }
                 });
             } else {
+                // Send the output to Watson as-is.
                 res.json(response);
             }
         }
     );
 });
 
+// This endpoint will fetch a weather report for the matched entities.
 app.post("/api/weather", async (req, res) => {
     const { entities = [] } = req.body;
 
+    // Validation. We can't get a weather report without data for it.
     if (!entities.length) {
         return res.status(400);
     }
 
     try {
+        // Get the weather report from the API, and return it.
         const weatherReport = await getWeatherReport(entities);
 
         return res.json(weatherReport);
     } catch (e) {
+        // Error handling.
         console.error(e);
         return res.status(500).json({
             messages: [
@@ -137,6 +184,8 @@ app.post("/api/weather", async (req, res) => {
     }
 });
 
+// Configure the port that the app will listen on...
 const port = process.env.PORT || process.env.VCAP_APP_PORT || 3000;
 
+// And start it. Once it's started, you'll see "Server listening on <port>" on the console.
 app.listen(port, () => console.log("Server listening on", port));
--- public/js/app.js
+++ public/js/app.js
@@ -1,29 +1,49 @@
+// Prevent globals by wrapping our code in a function.
+// Notice: We don't use document.ready, or DOMContentLoaded,
+// or equivalent, because this script is loaded asynchonously and
+// with defer.
 (function main() {
+    // Hold a reference to the current Watson context.
+    // Updated each time we get a response from Watson.
     let currentContext = null;
 
     /**
+     * Reference to the chat input.
+     *
      * @type {HTMLInputElement}
      */
     const input = document.querySelector("#chat-form-input");
 
+    /**
+     * Chat log.
+     *
+     * @type {HTMLDivElement}
+     */
     const chatLog = document.querySelector("#chat-log");
 
+    // Whenever the chat form submits, get the value from the input and
+    // send a message to our server.
+    // The server will then process the message and send it to Watson.
     document.querySelector("#chat-form").addEventListener("submit", async e => {
         e.preventDefault();
 
         const text = input.value.trim();
 
+        // Don't submit when there's no text.
         if (!text) {
             return;
         }
 
+        // Clear the input, and show the message on the chat log.
         input.value = "";
 
         printMessage(text, "mine");
 
+        // Send the message to Watson via the server.
         await sendMessageAndShowResult(text);
     });
 
+    // Prints the messages from Watson.
     async function parseWatsonMessage(data) {
         const { output = {} } = data;
 
@@ -32,13 +52,21 @@
         text.forEach(x => printMessage(x, "watson"));
     }
 
+    /**
+     * Gets the weather report when entities are matched by Watson.
+     * Likely inneficient, but it works.
+     *
+     * @param {{ entities: any[] }} watsonResponse
+     */
     async function getWeatherReport(watsonResponse) {
         const { entities } = watsonResponse;
 
+        // When there are no entities matched, do nothing.
         if (!entities.length) {
             return;
         }
 
+        // Get the weather.
         const response = await fetch("/api/weather", {
             method: "post",
             body: JSON.stringify({ entities }),
@@ -48,18 +76,26 @@
         });
 
         if (response.status === 200) {
+            // Everything went well, get the weather report and any messages,
+            // and show them on the screen.
             const { messages, weatherReport } = await response.json();
 
             messages.forEach(m => printMessage(m, "watson"));
 
             printWeatherReport(weatherReport);
         } else {
+            // Something went wrong.
             const { messages } = await response.json();
             messages.forEach(m => printMessage(m, "watson"));
         }
     }
 
+    /**
+     * Sends a message to Watson via the server and returns Watson's response.
+     * @param {string} text
+     */
     async function sendMessage(text) {
+        // Send the message...
         const response = await fetch("/api/message", {
             method: "post",
             body: JSON.stringify({ text: text, context: currentContext }),
@@ -71,6 +107,7 @@
         const data = await response.json();
 
         if (response.status === 200) {
+            // Save Watson's context for later...
             currentContext = data.context;
         } else {
             const text = await response.text();
@@ -81,6 +118,12 @@
         return data;
     }
 
+    /**
+     * Prints a message on the caht log.
+     *
+     * @param {string} text The message text.
+     * @param {string} who Who sent this message: watson or the user.
+     */
     function printMessage(text, who) {
         if (!text) {
             return;
@@ -95,6 +138,11 @@
         chatLog.appendChild(node);
     }
 
+    /**
+     * Prints a weather report on the chat log.
+     *
+     * @param {any} weatherReport
+     */
     function printWeatherReport(weatherReport) {
         const { item, units, location } = weatherReport;
         const [today, ...rest] = item.forecast;
@@ -113,6 +161,10 @@
         );
     }
 
+    /**
+     * Sends a message, waits for the response, and shows the response.
+     * @param {string} text
+     */
     async function sendMessageAndShowResult(text) {
         const message = await sendMessage(text);
 
@@ -121,5 +173,7 @@
         await getWeatherReport(message);
     }
 
+    // Kickstart Watson by sending it an empty message. Triggers the "welcome"
+    // node on the workspace.
     sendMessageAndShowResult("");
 })();
